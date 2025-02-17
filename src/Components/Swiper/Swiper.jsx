41ff915f86f8dd8579f0bf1f4c421a7a073a4cb9
Vinishaj@110502 <vinishajeyaramu@gmail.com>
2025-02-17 13:02:12 +0530

    swiper changes

--- src/Components/Swiper/Swiper.jsx
+++ src/Components/Swiper/Swiper.jsx
@@ -4,9 +4,8 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import { Autoplay, Pagination } from 'swiper/modules'
 import './Swiper.css'
-import slide1 from '../../Assets/Images/309947895_1278353496232931_1537738754282127533_n.jpeg'
 import slide2 from '../../Assets/Images/300602451_588076673026224_4504409814948100079_n.jpg'
-import slide3 from '../../Assets/Images/s.jpeg'
+
 const HomeSwiper = () => {
   return (
     <>
@@ -16,23 +15,22 @@ const HomeSwiper = () => {
           disableOnInteraction: false,
         }} modules={[Pagination,Autoplay]} className="mySwiper">
                 <SwiperSlide>
-                  
-                    <img src={slide1} alt=""  className='slide-1'/>
+                    <div className='slide-1'></div>
                     <h3 className='slide1-cont slide-text'>Style That Speaks – Transforming You, Effortlessly!</h3>
                   
                     
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={slide2} alt="" className='slide-2'/>
+                  <div className='slide-2'></div>
                     <h3 className='slide2-cont'>Create your Oun Style.</h3>
                     <p>Fresh perspectives, effortless transformations, and confidence-driven looks! Unleash your unique fashion statement with expert styling.</p>
-                    <button>Make an Appointment now!</button>
+                    <button className='btn'>Make an Appointment now!</button>
                 </SwiperSlide>
                
                 <SwiperSlide>
+                    <div className='slide-3'></div>
                     <h6 className='slide3-cont slide-text '> Discover fresh inspiration and create stunning new looks!.. </h6>
                     <span className='lpj slide-text'>with LabelPallavij</span>
-                    <img src={slide3} alt=""  className='slide-1'/>
                    
                 </SwiperSlide>
             </Swiper>
